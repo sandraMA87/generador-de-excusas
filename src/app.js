134e2bd2df1d10ed6c979792b490dbd2de096f10
sandraMA87 <sandra.moas@hotmail.com>
2023-03-07 21:29:44 +0000

    generador

--- src/app.js
+++ src/app.js
@@ -10,23 +10,27 @@ window.onload = function() {};
 
 //Definir la excusa
 let generateExcuse = () => {
-  let who = ["the dog", "my granma", "his turtle", "my bird"];
-  let what = ["ate", "pissed", "crushed", "broked"];
+  let who = ["Sandra", "Mi madre", "Ivan", "Sofía"];
+  let what = ["se enamoró", "comió", "disfrutó", "se cayó"];
   let when = [
-    "before the class",
-    "right in time",
-    "when I finished",
-    "during  my lunch",
-    "while I was praying"
+    "encima de un italiano",
+    "en una pizzería de Roma",
+    "en los canales venecianos",
+    "en el Coliseo",
+    "mientras rezaba "
   ];
+  let emoji = ["&#9996", "&#128165", "&#x1F60F", "&#x1F91D"];
 
   //Hacer que esa excusa muestre de forma aleatoria cada elemento del who, what, when
 
   let excuseWho = who[Math.floor(Math.random() * who.length)];
   let excuseWhat = what[Math.floor(Math.random() * what.length)];
   let excuseWhen = when[Math.floor(Math.random() * when.length)];
+  let excuseEmoji = emoji[Math.floor(Math.random() * emoji.length)];
 
-  return excuseWho + " " + excuseWhat + " " + excuseWhen + ".";
+  return (
+    excuseWho + " " + excuseWhat + " " + excuseWhen + " " + excuseEmoji + "."
+  );
 };
 
 document.querySelector("#excuse").innerHTML = generateExcuse();
