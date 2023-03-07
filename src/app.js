76d1b2491cd78d2e6535670296006e0ac622769b
sandraMA87 <sandra.moas@hotmail.com>
2023-03-07 09:57:04 +0000

    first

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//Crear una funcion
+window.onload = function() {};
+
+//Definir la excusa
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["ate", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during  my lunch",
+    "while I was praying"
+  ];
+
+  //Hacer que esa excusa muestre de forma aleatoria cada elemento del who, what, when
+
+  let excuseWho = who[Math.floor(Math.random() * who.length)];
+  let excuseWhat = what[Math.floor(Math.random() * what.length)];
+  let excuseWhen = when[Math.floor(Math.random() * when.length)];
+
+  return excuseWho + " " + excuseWhat + " " + excuseWhen + ".";
 };
+
+document.querySelector("#excuse").innerHTML = generateExcuse();
